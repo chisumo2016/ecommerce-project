805f44d378512a7ad7fec62288498ae6ee35f87f
bernard chisumo <bchisumo74@gmail.com>
2022-11-11 12:10:21 +0000

    Installed the axios package in vuejs project and configured

---
+++ backend/src/axios/axios.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import store from "../store";
+import router from "../router";
+import config from "tailwindcss/defaultConfig";
+
+/**Create axios Client*/
+const  axiosClient = axios.create({
+ /**Backend Laravel Url*/
+ baseURL : `${import.meta.env.VITE_API_BASE_URL}/api`
+});
+
+axiosClient.interceptors.request.use(config =>{
+    config.headers.Authorization = `Bearer ${store.state.user.token}`
+    return config;
+});
+
+axiosClient.interceptors.response.use(response =>{  // fullied or reject - promise
+    return response
+}, error => {
+    if (error.response.status === 401){
+        /**remove the token from session*/
+        sessionStorage.removeItem('TOKEN') //create thee  sessionStorage
+        router.push({name : 'login'})
+    }
+    throw  error;
+})
+
+export  default  axiosClient;
+
+
